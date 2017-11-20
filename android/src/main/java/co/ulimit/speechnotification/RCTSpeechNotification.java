257557d613e7924bad283d9a94500bf31b9a42d9
Andrey Starkov <im@andreystarkov.ru>
2017-11-20 14:59:44 +0400

    remove override

--- android/src/main/java/co/ulimit/speechnotification/RCTSpeechNotification.java
+++ android/src/main/java/co/ulimit/speechnotification/RCTSpeechNotification.java
@@ -27,12 +27,10 @@ public class RCTSpeechNotification implements ReactPackage {
     return modules;
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
 
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
   }
